daf8b8d15855d16734456cd196715260df044d9d
Ajith Naik <ajithnaik253@gmail.com>
2024-06-07 07:08:00 +0530

    Updated on Laptop

---
+++ IACSD/CricketManagement/src/com/app/tester/CricketerTest.java
@@ -0,0 +1,15 @@
+package com.app.tester;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.app.entity.Cricketer;
+
+public class CricketerTest {
+
+	public static void main(String[] args) {
+		 Map<String,Cricketer> cricktersmap = new HashMap<String, Cricketer>();
+
+	}
+
+}
